44fe24f5b2c5c7ece8bb510961508a8d67ccaad6
gauravgs <gauravngp7@gmail.com>
2020-03-04 17:49:42 +0530

    age and gender added

--- script.js
+++ script.js
@@ -4,17 +4,30 @@ Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
   faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
   faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-  faceapi.nets.faceExpressionNet.loadFromUri("/models")
+  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+  faceapi.nets.ageGenderNet.loadFromUri("/models")
 ]).then(startVideo);
 
+startVideo();
+
 function startVideo() {
+  /*
   navigator.getUserMedia(
     { video: {} },
-    stream => (video.srcObject = stream),
+    stream => video.srcObject = stream,
     err => console.error(err)
-  );
+  )
+  */
+
+  navigator.mediaDevices
+    .getUserMedia({
+      video: true,
+      audio: false
+    })
+    .then(cameraStream => {
+      video.srcObject = cameraStream;
+    });
 }
-startVideo();
 
 video.addEventListener("play", () => {
   const canvas = faceapi.createCanvasFromMedia(video);
@@ -25,11 +38,60 @@ video.addEventListener("play", () => {
     const detections = await faceapi
       .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
       .withFaceLandmarks()
-      .withFaceExpressions();
+      .withFaceExpressions()
+      .withAgeAndGender();
     const resizedDetections = faceapi.resizeResults(detections, displaySize);
     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
     faceapi.draw.drawDetections(canvas, resizedDetections);
     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+    resizedDetections.forEach(detection => {
+      const box = detection.detection.box;
+      const drawBox = new faceapi.draw.DrawBox(box, {
+        label: Math.round(detection.age) + " year old " + detection.gender
+      });
+      drawBox.draw(canvas);
+    });
   }, 100);
 });
+
+// const video = document.getElementById("video");
+
+// Promise.all([
+//   faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+//   faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+//   faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+//   faceapi.nets.faceExpressionNet.loadFromUri("/models")
+// ]).then(startVideo);
+
+// function startVideo() {
+//   navigator.getUserMedia(
+//     { video: {} },
+//     stream => (video.srcObject = stream),
+//     err => console.error(err)
+//   );
+// }
+// startVideo();
+
+// video.addEventListener("play", () => {
+//   const canvas = faceapi.createCanvasFromMedia(video);
+//   document.body.append(canvas);
+//   const displaySize = { width: video.width, height: video.height };
+//   faceapi.matchDimensions(canvas, displaySize);
+//   setInterval(async () => {
+//     // const detectionsWithAgeAndGender = await faceapi
+//     //   .detectAllFaces(input)
+//     //   .withFaceLandmarks()
+//     //   .withAgeAndGender();
+//     const detections = await faceapi
+//       .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+//       .withFaceLandmarks()
+//       .withFaceExpressions();
+//     const resizedDetections = faceapi.resizeResults(detections, displaySize);
+//     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+//     faceapi.draw.drawDetections(canvas, resizedDetections);
+//     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+
+//     faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+//   }, 100);
+// });
